58a91f7e1d0bf8b5896bcfb53cb1d60c7fca6ca0
niw0906 <niw0906@naver.com>
2025-12-06 02:02:45 +0900

    feat: 아이콘 텍스트 버튼 위젯 생성

---
+++ lib/widgets/buttons/icon_text_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class IconTextButton extends StatelessWidget {
+  const IconTextButton({
+    super.key,
+    this.bgColor,
+    this.fgColor,
+    required this.icon,
+    required this.label,
+    this.onPressed,
+  });
+
+  final Color? bgColor;
+  final Color? fgColor;
+  final Widget icon;
+  final String label;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: bgColor ?? Colors.white,
+        foregroundColor: fgColor ?? Colors.black,
+        disabledBackgroundColor: bgColor ?? Colors.white,
+        disabledForegroundColor: bgColor ?? Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      ),
+      onPressed: onPressed,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [icon, const SizedBox(width: 10), Text(label)],
+      ),
+    );
+  }
+}
