e8a8e437d13ed4422173ca726b26dda850c103f9
niw5924 <niw5924@naver.com>
2025-12-07 21:10:28 +0900

    pkg: 토스트 패키지 변경(toastification -> fluttertoast)

--- lib/utils/toast_utils.dart
+++ lib/utils/toast_utils.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:toastification/toastification.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ToastUtils {
   static void success(String message) {
-    toastification.show(
-      alignment: Alignment.bottomCenter,
-      autoCloseDuration: const Duration(seconds: 2),
-      type: ToastificationType.success,
-      style: ToastificationStyle.flat,
-      title: Text(message),
-      closeOnClick: true,
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.green,
+      textColor: Colors.white,
+      fontSize: 14,
     );
   }
 
   static void error(String message) {
-    toastification.show(
-      alignment: Alignment.bottomCenter,
-      autoCloseDuration: const Duration(seconds: 2),
-      type: ToastificationType.error,
-      style: ToastificationStyle.flat,
-      title: Text(message),
-      closeOnClick: true,
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 14,
     );
   }
 }
