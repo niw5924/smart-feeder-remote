b56c471ab3e757425851a940909d0d3978d76206
niw0906 <niw0906@naver.com>
2025-12-07 20:09:25 +0900

    feat: 로그아웃 버튼 추가

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../services/auth/auth_service.dart';
+import '../../widgets/buttons/app_text_button.dart';
+
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('설정 화면'));
+    return Center(
+      child: AppTextButton(
+        label: '로그아웃',
+        onPressed: () async {
+          await AuthService.signOut();
+        },
+      ),
+    );
   }
 }
