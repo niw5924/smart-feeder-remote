65b3df55ccdb8b4982711873298e0b57f7a561ac
niw5924 <niw5924@naver.com>
2025-12-07 20:06:01 +0900

    feat: 앱 공용 텍스트 버튼 위젯 생성

---
+++ lib/widgets/buttons/app_text_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppTextButton extends StatelessWidget {
+  const AppTextButton({
+    super.key,
+    this.bgColor,
+    this.fgColor,
+    required this.label,
+    this.onPressed,
+  });
+
+  final Color? bgColor;
+  final Color? fgColor;
+  final String label;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: bgColor ?? Colors.white,
+        foregroundColor: fgColor ?? Colors.black,
+        disabledBackgroundColor: bgColor ?? Colors.white,
+        disabledForegroundColor: fgColor ?? Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      ),
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
