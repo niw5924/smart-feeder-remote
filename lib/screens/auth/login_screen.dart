8194ce760a74ef9630a21d4771b2ab1016b34b7f
niw5924 <niw5924@naver.com>
2025-12-07 19:24:45 +0900

    utils: 토스트 유틸 생성

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../services/auth/auth_service.dart';
+import '../../utils/toast_utils.dart';
 import '../../widgets/buttons/icon_text_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -24,7 +25,7 @@ class LoginScreen extends StatelessWidget {
               try {
                 await AuthService().signInWithGoogle();
               } catch (e) {
-                print(e);
+                ToastUtils.error('로그인에 실패했어요. 다시 시도해 주세요.');
               }
             },
           ),
