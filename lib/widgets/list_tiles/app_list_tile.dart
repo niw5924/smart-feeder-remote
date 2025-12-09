713e49a446ee03d4dbfc0e0faa7807c01bba18e9
niw5924 <niw5924@naver.com>
2025-12-09 22:11:16 +0900

    feat: 피드 화면 UI

--- lib/widgets/list_tiles/app_list_tile.dart
+++ lib/widgets/list_tiles/app_list_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/app_colors.dart';
+
 class AppListTile extends StatelessWidget {
   const AppListTile({
     super.key,
@@ -8,15 +10,28 @@ class AppListTile extends StatelessWidget {
     this.onTap,
   });
 
-  final Widget title;
-  final Widget? subtitle;
+  final String title;
+  final String? subtitle;
   final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: title,
-      subtitle: subtitle,
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: AppColors.textPrimary,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      subtitle:
+          subtitle != null
+              ? Text(
+                subtitle!,
+                style: const TextStyle(color: AppColors.textSecondary),
+              )
+              : null,
       trailing: onTap != null ? const Icon(Icons.chevron_right) : null,
       onTap: onTap,
     );
