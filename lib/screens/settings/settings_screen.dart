a1d5dbae4887ff96dd7d1a0a77f0d07915935009
niw0906 <niw0906@naver.com>
2025-12-13 16:49:17 +0900

    chore: 설정화면 기본 UI

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
 
-import '../../services/auth/auth_service.dart';
-import '../../widgets/buttons/app_text_button.dart';
+import '../../constants/app_colors.dart';
+import '../../widgets/cards/app_card.dart';
+import '../../widgets/list_tiles/app_list_tile.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AppTextButton(
-        label: '로그아웃',
-        onPressed: () async {
-          await AuthService.signOut();
-          Phoenix.rebirth(context);
-        },
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          AppCard(
+            color: AppColors.secondary,
+            borderRadius: 0,
+            child: AppListTile(title: '계정', onTap: () {}),
+          ),
+          const Divider(color: AppColors.divider),
+        ],
       ),
     );
   }
