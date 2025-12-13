18830fe8f8ac3bef561968929debf30d9f154c18
niw0906 <niw0906@naver.com>
2025-12-13 17:08:31 +0900

    chore: 계정화면 Scaffold

--- lib/screens/settings/account/account_screen.dart
+++ lib/screens/settings/account/account_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 
+import '../../../constants/app_colors.dart';
 import '../../../services/auth/auth_service.dart';
 import '../../../widgets/buttons/app_text_button.dart';
 
@@ -9,13 +10,29 @@ class AccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AppTextButton(
-        label: '로그아웃',
-        onPressed: () async {
-          await AuthService.signOut();
-          Phoenix.rebirth(context);
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '계정',
+          style: TextStyle(
+            color: AppColors.textPrimary,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: AppColors.appBarBackground,
+        iconTheme: const IconThemeData(color: AppColors.textPrimary),
+      ),
+      backgroundColor: AppColors.scaffoldBackground,
+      body: SafeArea(
+        child: Center(
+          child: AppTextButton(
+            label: '로그아웃',
+            onPressed: () async {
+              await AuthService.signOut();
+              Phoenix.rebirth(context);
+            },
+          ),
+        ),
       ),
     );
   }
