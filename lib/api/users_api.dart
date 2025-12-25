7c02167925f8e535f7860fe0f9ee20d08ca4232b
niw5924 <niw5924@naver.com>
2025-12-25 21:54:42 +0900

    feat: users_api 생성 및 upsertMe 메소드 추가

---
+++ lib/api/users_api.dart
@@ -0,0 +1,23 @@
+import 'dio_client.dart';
+
+class UsersApi {
+  /// 유저 생성/갱신 요청
+  static Future<Map<String, dynamic>> upsertMe({
+    required String provider,
+    required String providerUserId,
+    String? nickname,
+    String? profileImageUrl,
+  }) async {
+    final response = await DioClient.dio.post(
+      '/api/users/me',
+      data: {
+        'provider': provider,
+        'providerUserId': providerUserId,
+        'nickname': nickname,
+        'profileImageUrl': profileImageUrl,
+      },
+    );
+
+    return response.data;
+  }
+}
