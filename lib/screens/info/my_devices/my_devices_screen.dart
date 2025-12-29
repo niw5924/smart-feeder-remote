0d6026320ecf06abc9ab6e38cd3cfaa7960d5fd3
niw0906 <niw0906@naver.com>
2025-12-29 22:13:42 +0900

    feat: 기기 상세 화면 구현

--- lib/screens/info/my_devices/my_devices_screen.dart
+++ lib/screens/info/my_devices/my_devices_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:smart_feeder_remote/providers/device/device_list_provider.dart';
 
 import '../../../theme/app_colors.dart';
@@ -30,6 +31,9 @@ class MyDevicesScreen extends ConsumerWidget {
                     child: AppListTile(
                       title: device.deviceName,
                       subtitle: '${device.deviceId} • ${device.location}',
+                      onTap: () {
+                        context.push('/device_detail', extra: device);
+                      },
                     ),
                   );
                 },
