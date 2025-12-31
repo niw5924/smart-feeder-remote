4d821e9650a37199e69bf036d23cbcd0c5967cd3
niw0906 <niw0906@naver.com>
2025-12-31 23:40:08 +0900

    chore: 앱 공용 버튼 width 인자 추가

--- lib/widgets/buttons/app_icon_text_button.dart
+++ lib/widgets/buttons/app_icon_text_button.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppIconTextButton extends StatelessWidget {
   const AppIconTextButton({
     super.key,
+    this.width,
     this.bgColor,
     this.fgColor,
     required this.icon,
@@ -10,6 +11,7 @@ class AppIconTextButton extends StatelessWidget {
     this.onPressed,
   });
 
+  final double? width;
   final Color? bgColor;
   final Color? fgColor;
   final Widget icon;
@@ -18,19 +20,24 @@ class AppIconTextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: bgColor ?? Colors.white,
-        foregroundColor: fgColor ?? Colors.black,
-        disabledBackgroundColor: bgColor ?? Colors.white,
-        disabledForegroundColor: fgColor ?? Colors.black,
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      ),
-      onPressed: onPressed,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [icon, const SizedBox(width: 10), Text(label)],
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: bgColor ?? Colors.white,
+          foregroundColor: fgColor ?? Colors.black,
+          disabledBackgroundColor: bgColor ?? Colors.white,
+          disabledForegroundColor: fgColor ?? Colors.black,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [icon, const SizedBox(width: 10), Text(label)],
+        ),
       ),
     );
   }
--- lib/widgets/buttons/app_text_button.dart
+++ lib/widgets/buttons/app_text_button.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class AppTextButton extends StatelessWidget {
   const AppTextButton({
     super.key,
+    this.width,
     this.bgColor,
     this.fgColor,
     required this.label,
     this.onPressed,
   });
 
+  final double? width;
   final Color? bgColor;
   final Color? fgColor;
   final String label;
@@ -16,17 +18,22 @@ class AppTextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: bgColor ?? Colors.white,
-        foregroundColor: fgColor ?? Colors.black,
-        disabledBackgroundColor: bgColor ?? Colors.white,
-        disabledForegroundColor: fgColor ?? Colors.black,
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: bgColor ?? Colors.white,
+          foregroundColor: fgColor ?? Colors.black,
+          disabledBackgroundColor: bgColor ?? Colors.white,
+          disabledForegroundColor: fgColor ?? Colors.black,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(label),
       ),
-      onPressed: onPressed,
-      child: Text(label),
     );
   }
 }
