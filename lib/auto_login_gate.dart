109c413c712289d2f323de7272a4f64b5ed5057e
niw5924 <niw5924@naver.com>
2026-01-03 02:49:27 +0900

    feat: 로그인/자동로그인 시 대표기기 MQTT 토픽 자동 구독 적용

--- lib/auto_login_gate.dart
+++ lib/auto_login_gate.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'services/auth/auth_service.dart';
+import 'utils/log_utils.dart';
 import 'utils/toast_utils.dart';
 import 'utils/user_data_sync.dart';
 
@@ -29,7 +30,9 @@ class _AutoLoginGateState extends ConsumerState<AutoLoginGate> {
     if (user != null) {
       try {
         await loadDevices(ref);
+        initMqttSub(ref);
       } catch (e) {
+        LogUtils.e(e);
         ToastUtils.error('데이터를 불러오지 못했습니다.');
       }
     }
