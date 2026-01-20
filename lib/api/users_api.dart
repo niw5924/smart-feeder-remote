9fa58f7cc6f5c0fd8af689de6c0a5d4608d0f0de
niw0906 <niw0906@naver.com>
2026-01-20 23:56:22 +0900

    chore: upsertMe 메소드 주석 추가

--- lib/api/users_api.dart
+++ lib/api/users_api.dart
@@ -1,7 +1,8 @@
 import 'dio_client.dart';
 
 class UsersApi {
-  /// 유저 생성/갱신 요청
+  /// provider/providerUserId는 미들웨어에서 자동으로 생성/조회되므로 요청에 포함하지 않습니다.
+  /// 내 프로필(닉네임/프로필 이미지) 정보 저장/갱신 요청
   static Future<Map<String, dynamic>> upsertMe({
     String? nickname,
     String? profileImageUrl,
