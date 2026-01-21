165e84985e26bb9794de0c625364b0c6e6c32114
niw0906 <niw0906@naver.com>
2026-01-21 23:22:49 +0900

    chore: getToken 주석 제거

--- lib/services/notifications/fcm_notification_service.dart
+++ lib/services/notifications/fcm_notification_service.dart
@@ -9,9 +9,6 @@ class FcmNotificationService {
   FcmNotificationService._();
 
   static Future<void> init() async {
-    final token = await FirebaseMessaging.instance.getToken();
-    LogUtils.d('fcmToken=$token');
-
     /// 포그라운드에선 FCM이 시스템 알림을 자동으로 띄우지 않아서 로컬 알림으로 직접 표시
     FirebaseMessaging.onMessage.listen((message) async {
       LogUtils.d('onMessage data=${message.data}');
