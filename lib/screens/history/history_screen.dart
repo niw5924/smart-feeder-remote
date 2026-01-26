1f37e871834fffc9b64efb4750b4b9f8ab53f94a
niw0906 <niw0906@naver.com>
2026-01-26 23:28:48 +0900

    feat: 기록 화면 MqttLogProvider 표시

--- lib/screens/history/history_screen.dart
+++ lib/screens/history/history_screen.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:smart_feeder_remote/api/mqtt_logs_api.dart';
-import 'package:smart_feeder_remote/widgets/buttons/app_icon_text_button.dart';
+import 'package:smart_feeder_remote/providers/mqtt_log/mqtt_log_list_provider.dart';
+import 'package:smart_feeder_remote/theme/app_colors.dart';
+import 'package:smart_feeder_remote/widgets/cards/app_card.dart';
+import 'package:smart_feeder_remote/widgets/list_tiles/app_list_tile.dart';
 
 class HistoryScreen extends ConsumerWidget {
   const HistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: AppIconTextButton(
-        width: double.infinity,
-        icon: const Icon(Icons.list_alt),
-        label: '기록 불러오기',
-        onPressed: () async {
-          await MqttLogsApi.all();
-        },
-      ),
-    );
+    final mqttLogs = ref.watch(mqttLogListProvider);
+
+    return mqttLogs.isEmpty
+        ? const Center(child: Text('기록이 없습니다.'))
+        : ListView.separated(
+            padding: const EdgeInsets.all(16),
+            separatorBuilder: (_, __) => const SizedBox(height: 16),
+            itemCount: mqttLogs.length,
+            itemBuilder: (context, index) {
+              final mqttLog = mqttLogs[index];
+
+              return AppCard(
+                color: AppColors.cardPrimary,
+                child: AppListTile(
+                  title: mqttLog.topic,
+                  subtitle: '${mqttLog.deviceId} • ${mqttLog.receivedAt}',
+                ),
+              );
+            },
+          );
   }
 }
